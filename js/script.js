e4d4b97a02f814d27b131682095e228fc0013dd3
Ravi Kumar <ravikumardrsdrs@gmail.com>
2018-11-15 23:23:27 +0530

    Adding dark navbar in mobile device and medium device

--- js/script.js
+++ js/script.js
@@ -122,4 +122,10 @@ $(document).ready(function() {
       }
     });
   });
+
+  $(document).ready(function() {
+    $('.navbar-nav li a').click(function(event) {
+      $('.navbar-collapse').collapse('hide');
+    });
+  });
 });
